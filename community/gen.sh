669d87349827fcaf1eb8569685f957badaa36ff0
zyno1 <olivierzeyen@gmail.com>
2023-05-02 10:44:49 +0200

    scripts

---
+++ community/gen.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+nv=75
+nc=750
+nb=5
+
+k=3
+Q="$1" #0.3 -> 0.8
+comm=8
+
+for ic in $(seq 1 "$nc") ; do
+    for j in $(seq 1 "$nb") ; do
+        fname="$ic.$j.cnf"
+        commAttach -n $nv -m $ic -c $comm -Q $Q -k $k -o "$fname" -s "$RANDOM"
+
+        r=$(z3 -dimacs "$fname")
+        unsat=$(echo "$r" | grep -E "^s UNSATISFIABLE$")
+        if [ -n "$unsat" ] ; then
+            rm "$fname"
+        fi
+    done
+done
